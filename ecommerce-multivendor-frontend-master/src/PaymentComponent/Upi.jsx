f05083d496d25376397aa724b91d31ea7ffd120f
Liku-Swain <likuswain@rjaytechnologies.com>
2024-04-24 18:24:51 +0530

    Upi Image add

--- ecommerce-multivendor-frontend-master/src/PaymentComponent/Upi.jsx
+++ ecommerce-multivendor-frontend-master/src/PaymentComponent/Upi.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-
-import '../App.css';
-
+ import '../App.css';
+import upiLogo from "../images/upi-logo.png";
+import { useLocation } from "react-router-dom";
 const Upi = () => {
-    
+  const location = useLocation();
+   const priceToPay = location.state.priceToPay;
+
   return (
     <div className="upi-box">
-      <img src='images\upi-logo.png' alt="UPI Logo" className="upi-logo" />
-      <input type="text" placeholder="Enter UPI ID" className="upi-input" />
-      <button className="upi-button">Pay</button>
+      <img
+        src={upiLogo}
+        alt="UPI Logo"
+        width="70px"
+        height="70px"
+        className="upi-logo"
+      />
+      <input
+        type="text"
+        placeholder="  Enter Your UPI ID "
+        className="upi-input"
+      />
+      <button className="upi-button">Pay {priceToPay}</button>
     </div>
   );
 }
